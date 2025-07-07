c0f7667398fba9f1a67ebf4c563e6816794bda6d
defnothai <honghaiit257@gmail.com>
2025-07-08 00:40:45 +0700

    Add api upload images to cloudinary

--- src/main/java/com/fuhcm/swp391/be/itmms/service/UploadImageFile.java
+++ src/main/java/com/fuhcm/swp391/be/itmms/service/UploadImageFile.java
@@ -3,9 +3,11 @@ package com.fuhcm.swp391.be.itmms.service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 
 public interface UploadImageFile {
 
     String uploadImage(MultipartFile file) throws IOException;
+    List<String> uploadImages(List<MultipartFile> files) throws IOException;
 
 }
