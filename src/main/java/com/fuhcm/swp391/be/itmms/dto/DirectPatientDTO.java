394674b5d20dd23a3e1a9984b316013e30c45b3d
defnothai <honghaiit257@gmail.com>
2025-07-29 00:17:14 +0700

    change valid direct dto

--- src/main/java/com/fuhcm/swp391/be/itmms/dto/DirectPatientDTO.java
+++ src/main/java/com/fuhcm/swp391/be/itmms/dto/DirectPatientDTO.java
@@ -11,7 +11,6 @@ import java.time.LocalDate;
 @Data
 public class DirectPatientDTO extends AccountCreateRequest {
 
-    @NotBlank(message = "Địa chỉ không được để trống", groups = OnUpdate.class)
     @Size(max = 255, message = "Địa chỉ tối đa 255 ký tự", groups = OnUpdate.class)
     @Size(min = 10, message = "Địa chỉ ít nhất 10 ký tự" , groups = OnUpdate.class)
     @Column(name = "Address", nullable = true, length = 255, columnDefinition = "NVARCHAR(255)")
@@ -22,14 +21,12 @@ public class DirectPatientDTO extends AccountCreateRequest {
     @Column(name = "DayOfBirth", nullable = true)
     private LocalDate dob;
 
-    @NotBlank(message = "Số CMND/CCCD không được để trống", groups = OnUpdate.class)
     @Size(max = 15, message = "Số CMND/CCCD tối đa 15 ký tự", groups = OnUpdate.class)
     @Size(min = 9, message = "Số CMND/CCCD ít nhất 9 ký tự", groups = OnUpdate.class)
     @Pattern(regexp = "\\d{9,15}", message = "Số CMND/CCCD chỉ được chứa chữ số", groups = OnUpdate.class)
     @Column(name = "IndentityNumber", nullable = true, length = 15)
     private String identityNumber;
 
-    @NotBlank(message = "Quốc tịch không được để trống" , groups = OnUpdate.class)
     @Size(max = 15, message = "Quốc tịch tối đa 15 ký tự", groups = OnUpdate.class)
     @Size(min = 2, message = "Quốc tịch ít nhất 2 ký tự", groups = OnUpdate.class)
     @Column(name = "Nationality", nullable = true, length = 15, columnDefinition = "NVARCHAR(50)")
